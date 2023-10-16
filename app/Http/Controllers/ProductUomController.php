5819f487d610f640571ab3d746f75bed7f0315f9
johnathan221998 <thachson009@gmail.com>
2023-10-16 18:19:15 +0700

    first commit

---
+++ app/Http/Controllers/ProductUomController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ProductUom;
+
+class ProductUomController extends Controller
+{
+    public function index()
+    {
+        $product_uoms = ProductUom::paginate(3);
+        return view('products.index', ['product_uoms'=>$product_uoms]);
+        
+    }
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'product_code' => 'required|unique:products',
+            'uom' => 'required',
+            'description' => 'required',
+            'unit_cost' => 'required|numeric',
+            'sell_price' => 'required|numeric',
+            'discount' => 'nullable|numeric',
+        ]);
+        Uom::create($validatedData);
+        return redirect('product')->with('flash_message', 'Product Addess!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $uoms = Uom::find($id);
+        if(!$product){
+            abort(404);
+        }
+        return view('products.show')->with('products', $uoms);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $uoms = Uom::find($id);
+        if(!$uoms){
+            abort(404);
+        }
+        return view('uom.edit')->with('uoms', $uoms);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $uoms = Uom::find($id);
+        $input = $request->all();
+        return redirect('product')->with('flash_message', 'Product Updated!'); 
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $uoms = Uom::find($id);
+        if (!$uoms) {
+            abort(404);
+        }
+        $uoms->delete();
+        return redirect('product')->with('flash_message', 'Product Deleted!'); 
+    }
+}
